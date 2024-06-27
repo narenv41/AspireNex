250b65abfc3801873197514ba12e9ae3a9d72667
Velayudham Narendharan <67109518+narenv41@users.noreply.github.com>
2024-06-27 15:10:05 +0530

    Add files via upload

---
+++ components/data.tsx
@@ -0,0 +1,8 @@
+// constants.ts
+export const navItems = [
+    { name: "Home", link: "#start" },
+    { name: "Education", link: "#exp" },
+    { name: "Projects", link: "#project" },
+    { name: "Contact", link: "#footer" },
+  ];
+  
